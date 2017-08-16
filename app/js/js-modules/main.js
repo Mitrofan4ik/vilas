3c3c10620edc33b517f49fbb98e50eb359bfb6cc
Mitrofan4ik <ekaterynamytrofanova@gmail.com>
2017-08-17 01:26:50 +0300

    add about us page

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -2,10 +2,20 @@
 //= js-assets/variables
 //= js-assets/functions
 jQuery( document ).ready(function() {
+	// ATIVE LIST ITEM
+	$('.jsNavList').on('click', 'li', function(event) {
+		event.preventDefault();
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+	// ATIVE LIST ITEM
 	// SHOW NAV MENU
 	$('.jsNav').on('click', '.jsTrigger', function() {
 		$(this).toggleClass('active');
+		if (true) {
+			$('.jsNavList').show(1000);
+		} else {
+			$('.jsNavList').hide(1000);
+		}
 	});
 	// SHOW NAV MENU
-  
 });
